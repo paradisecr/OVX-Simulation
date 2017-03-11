c1a4d53b03b55b6f4df4a80704f78bff6a816d8e
paradisecr <704645806@qq.com>
2017-03-11 18:34:56 +0800

    Revert "Add search backtrace, optimize search process use iteration."
    This reverts commit c06eacd6d24d5d2ff26c4fb1a1c4f50a80d5488c.

--- src/main/java/net/fnl/ovx/OVXApplication.java
+++ src/main/java/net/fnl/ovx/OVXApplication.java
@@ -6,7 +6,6 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import org.jgrapht.GraphPath;
 import org.jgrapht.UndirectedGraph;
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.alg.shortestpath.KShortestPaths;
 import org.jgrapht.graph.SimpleGraph;
 
@@ -141,9 +140,8 @@ public class OVXApplication {
          * @return
          */
         public boolean allocate(){
-//            while (!unhandledVirtualVertexSet.isEmpty() && (isAllocateSuccess = allocateNextVirtualVertex())) {
-//            }
-            isAllocateSuccess = allocateInternal();
+            while (!unhandledVirtualVertexSet.isEmpty() && (isAllocateSuccess = allocateNextVirtualVertex())) {
+            }
             // 映射失败的情况
             if (!isAllocateSuccess) {
                 resourceAllocation = new ResourceAllocation(resourceRequest, new HashMap<String, String>(), new HashMap<Edge, GraphPath<String, Edge>>(), isAllocateSuccess);
@@ -181,137 +179,6 @@ public class OVXApplication {
 
         public ResourceAllocation getResourceAllocation() {
             return resourceAllocation;
-        }
-
-        public boolean allocateInternal() {
-            // 参数：已处理节点集合，未处理节点集合，请求集合，已分配资源，剩余资源
-            // ？？何时扣减资源？ 何时退还资源？
-            // 1. 取一个未映射的虚拟节点V
-            //    取所有虚拟链路
-            // 2. 取所有可映射的物理节点列表:phyList
-            // 3. 遍历物理节点列表，计算成本，并按照成本进行排序
-            // 4. 按照排序后的phyList, 逐一尝试映射(迭代调用分配函数)，(扣资源，调用，还资源)
-            //      若成功，return true;
-            //      若失败，下一个
-            //      若已空，return false;
-
-            // 1. 取一个未映射的虚拟节点V
-            String vVertex = getRelatedUnhandledvertex();
-            if (null == vVertex) return true;
-            //    取所有虚拟链路
-            List<Edge> relatedVirtualEdgeList = getRelatedVirtualEdge(vVertex);
-            // 2. 取所有可映射的物理节点列表:phyList
-            List<String> availablePhysicalvertexList = getAvailablePhysicalvertexList(resourceRequest.getVertexRequest(vVertex));
-            // 判断可用物理节点是否为空
-            if (availablePhysicalvertexList.isEmpty()) {
-                return false;
-            }
-            // 3. 遍历物理节点列表，计算成本，并按照成本进行排序，在此期间排除掉不符合资源要求的物理节点
-            //    并用Map存储每个物理节点的path、总cost
-            Map<String, Map<Edge, GraphPath<String, Edge>>> nodesResult = new HashMap<String, Map<Edge, GraphPath<String, Edge>>>();  //每个物理节点对应的(edge--->path)
-            final Map<String, Long> resultCosts = new HashMap<String, Long>();    //每个节点对应的总成本
-            List<String> lastAvPhyVertexList = new ArrayList<String>();
-            for (String physicalvertex : availablePhysicalvertexList) {
-                Map<Edge, GraphPath<String, Edge>> edgeMap = new HashMap<Edge, GraphPath<String, Edge>>();  //暂存当前节点的路径映射
-                boolean isPhysicalVertexAvailable = true;                                                  //记录当前物理节点是否可用，默认为可用
-                final Map<String, Long> vAvailableResourceTmp = cloneResource(vAvailableResourceMapCache); //计算时需要尝试扣减资源
-                final Map<Edge, Long> eAvailableResourceTmp = cloneResource(eAvailableResourceMapCache);
-                //  对每个虚拟链路尝试最短路映射
-                for (final Edge virtualEdge : relatedVirtualEdgeList) {
-                    String virtualSrcVertex = virtualGraph.getEdgeSource(virtualEdge);
-                    String virtualDstVertex = virtualGraph.getEdgeTarget(virtualEdge);
-                    String phySrcVertex = null;;
-                    String phyDstVertex = null;
-                    if((phyDstVertex = vAllocationMapCache.get(virtualDstVertex))!= null) {
-                        phySrcVertex = physicalvertex;
-                    } else {
-                        phySrcVertex =  vAllocationMapCache.get(virtualSrcVertex);
-                        phyDstVertex = physicalvertex;
-                    }
-                    DijkstraShortestPath djPathService = new DijkstraShortestPath(physicalGraph);
-                    GraphPath<String, Edge> path = djPathService.getPath(phySrcVertex, phyDstVertex);
-                    if (null == path) {   //当前虚拟路径映射不存在，说明此物理节点不符合要求
-                        isPhysicalVertexAvailable = false;
-                        break;
-                    }
-                    // 扣减临时路径上链路资源，并记录链路映射
-                    edgeMap.put(virtualEdge, path);
-                    long request = resourceRequest.getEdgeRequest(virtualEdge);
-                    for (Edge edge : path.getEdgeList()) {
-                        long remain = eAvailableResourceTmp.get(edge);
-                        remain -= request;
-                        eAvailableResourceTmp.put(edge, remain);
-                    }
-                }
-                // 判断当前物理节点是否符合节点要求，若符合要求：记录物理链路的 相关虚拟路径映射Map和Cost
-                if (!isPhysicalVertexAvailable) continue;
-                long cost = caculateCost(physicalvertex, resourceRequest.getVertexRequest(vVertex),
-                        edgeMap);
-                resultCosts.put(physicalvertex, cost);  // 记录总cost
-                nodesResult.put(physicalvertex, edgeMap);  // 记录链路映射
-                lastAvPhyVertexList.add(physicalvertex);  // 将当前物理节点加入可用列表
-            }
-            // 4. 按成本排序phyList, 逐一尝试映射(迭代调用分配函数)，(扣资源，调用，还资源)
-            Collections.sort(lastAvPhyVertexList, new Comparator<String>() {       //按照成本对可用物理节点进行排序
-                public int compare(String o1, String o2) {
-                    long c1 = resultCosts.get(o1);
-                    long c2 = resultCosts.get(o2);
-                    if (c1 < c2) {
-                        return -1;
-                    } else if (c1 > c2){
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                }
-            });
-            for (String phyV : lastAvPhyVertexList) {
-                // 扣减资源
-                vAllocationMapCache.put(vVertex, phyV);  //在cache中记录此次链路和节点映射
-                eAllocationMapCache.putAll(nodesResult.get(phyV));
-                Map<Edge, GraphPath<String, Edge>> pathMap = nodesResult.get(phyV);;
-                for (Edge vEdge : pathMap.keySet()) {                        //从cache中扣减链路资源
-                    GraphPath<String, Edge> path = pathMap.get(vEdge);
-                    long request = resourceRequest.getEdgeRequest(vEdge);
-                    for (Edge edge : path.getEdgeList()) {
-                        long remain = eAvailableResourceMapCache.get(edge);
-                        remain -= request;
-                        eAvailableResourceMapCache.put(edge, remain);
-                    }
-                }
-                long request = resourceRequest.getVertexRequest(vVertex);      ///从cache中扣减节点资源
-                long remain = vAvailableResourceMapCache.get(phyV);
-                vAvailableResourceMapCache.put(phyV, remain - request);
-                handledVirtualVertexSet.add(vVertex);
-                unhandledVirtualVertexSet.remove(vVertex);
-                // TODO: 映射
-                boolean isSubAllocateSuccess = allocateInternal();
-                // 若成功，返回true
-                if (isSubAllocateSuccess) return true;
-                // 若失败，返还资源，continue下一个节点
-                vAllocationMapCache.remove(vVertex);  //在cache中删除此次链路和节点映射记录
-                Map<Edge, GraphPath<String, Edge>> delEdgeMap = nodesResult.get(phyV);
-                for (Edge edge : delEdgeMap.keySet()) {
-                    eAllocationMapCache.remove(edge);
-                }
-                for (Edge vEdge : pathMap.keySet()) {                        //从cache中扣减链路资源
-                    GraphPath<String, Edge> path = pathMap.get(vEdge);
-                    long backRequest = resourceRequest.getEdgeRequest(vEdge);
-                    for (Edge edge : path.getEdgeList()) {
-                        long curRemain = eAvailableResourceMapCache.get(edge);
-                        curRemain += backRequest;
-                        eAvailableResourceMapCache.put(edge, curRemain);
-                    }
-                }
-                vAvailableResourceMapCache.put(phyV, remain);
-                handledVirtualVertexSet.remove(vVertex);
-                unhandledVirtualVertexSet.add(vVertex);
-                return false;
-            }
-            //      若成功，return true;
-            //      若失败，下一个
-            //      若已空，return false;
-            return false;
         }
 
         private boolean allocateNextVirtualVertex() {
